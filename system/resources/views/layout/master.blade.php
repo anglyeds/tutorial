9706567feab69378afd722d960709a1a70d5180c
JEFFREY <anglyeds@gmail.com>
2017-03-15 19:05:01 +0800

    initial

---
+++ system/resources/views/layout/master.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Merch</title>
+
+    <!-- Bootstrap -->
+    <link href="system/node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="system/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+
+
+  </head>
+  <body>
+  @yield('content')
+
+  </body>
+</html>
+</html>
---
+++ system/resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+@extends('layout.master')
+
+@section('content')
+
+<div class=container>
+<div class="row">
+    <h1 class="text-center">Welcome To Merchandiser Web</h1>
+    <div class="col-md-4">
+     
+     </div>
+    <div class="col-md-4">
+
+    </div>
+    <div class="col-md-4">
+     
+    </div>
+</div>
+</div>
+
+@endsection
